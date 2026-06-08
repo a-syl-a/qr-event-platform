6f3dfdccf4f0ed75b6af91c6176eb7ca04a6ae15
Lyra Jemella <144532368+a-syl-a@users.noreply.github.com>
2026-06-08 08:06:17 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/qr-event-platform/',
+  server: {
+    port: 5173,
+    host: true
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  }
 })
